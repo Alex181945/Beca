239936c7b815e61f1dd15e0d872518a5961c98b6
VAIO-PC <VAIO-PC@Adriana>
2016-11-14 16:52:23 -0600

    ok

--- Beca/src/com/example/beca/MainActivity.java
+++ Beca/src/com/example/beca/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.beca;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private EditText et1,et2,et3;
+	private TextView tv1,tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //tv1 = (TextView)findViewById(R.id.)
+        
     }
 
 
@@ -18,6 +27,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void cambioPantalla(){
+    	Intent x =new Intent (this,InicioActivity.class);
+    	startActivity(x);
     }
     
 }
