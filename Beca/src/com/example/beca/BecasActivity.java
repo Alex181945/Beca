9778f97458c094a4ca7fa400ae9811e288a16d73
Alex181945 <alestra55@hotmail.com>
2016-11-16 00:25:37 -0600

    OK

---
+++ Beca/src/com/example/beca/BecasActivity.java
@@ -0,0 +1,24 @@
+package com.example.beca;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BecasActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_becas);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.becas, menu);
+		return true;
+	}
+
+
+}
