ca5e387c360d6a7422d17066caa4d5cd20500208
Alex181945 <alestra55@hotmail.com>
2016-11-16 00:17:55 -0600

    ok

--- Beca/src/com/example/beca/InicioActivity.java
+++ Beca/src/com/example/beca/InicioActivity.java
@@ -3,13 +3,19 @@ package com.example.beca;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class InicioActivity extends Activity {
+	
+	TextView tv2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inicio);
+		
+		tv2 = (TextView) findViewById(R.id.tv2);
 	}
 
 	@Override
@@ -17,6 +23,18 @@ public class InicioActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.inicio, menu);
 		return true;
+	}
+	
+	public void Info(View view){
+		
+		String a = "La Coordinación Nacional de Becas de Educación Superior (CNBES) C O N V O C A N,"
+				+ " a los/las estudiantes de Licenciatura y Técnico Superior Universitario (TSU)"
+				+ " que hayan ingresado o se encuentren realizando estudios en "
+				+ "Instituciones Públicas de Educación Superior (IPES) en México "
+				+ "para que obtengan una beca de Manutención.";
+		
+		tv2.setText(a);
+		
 	}
 
 }
