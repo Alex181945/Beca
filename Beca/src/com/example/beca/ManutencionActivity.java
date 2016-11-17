c12a1c8423838337870ac8ac6384c3e7a5b28b3c
Alex181945 <alestra55@hotmail.com>
2016-11-16 18:59:14 -0600

    pantallas de manutención

---
+++ Beca/src/com/example/beca/ManutencionActivity.java
@@ -0,0 +1,41 @@
+package com.example.beca;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class ManutencionActivity extends Activity {
+	
+	TextView  tv2;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_manutencion);
+		tv2 = (TextView)findViewById(R.id.tv2);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.manutencion, menu);
+		return true;
+	}
+	
+	public void cambioPantalla(View view){
+    	Intent x =new Intent (this,InicioActivity.class);
+    	startActivity(x);
+    }
+	
+	public void req(View view){
+		
+		tv2.setText("");
+		
+	}
+	
+
+}
