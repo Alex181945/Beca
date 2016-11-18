b2ee27f515add719aa794f3403e0c6fe9c08de00
VAIO-PC <VAIO-PC@Adriana>
2016-11-17 18:20:36 -0600

    creación de ventana de tips

--- Beca/gen/com/example/beca/R.java
+++ Beca/gen/com/example/beca/R.java
@@ -47,12 +47,14 @@ public final class R {
         public static final int activity_inicio=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_manutencion=0x7f030003;
+        public static final int activity_tips=0x7f030004;
     }
     public static final class menu {
         public static final int becas=0x7f070000;
         public static final int inicio=0x7f070001;
         public static final int main=0x7f070002;
         public static final int manutencion=0x7f070003;
+        public static final int tips=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -66,7 +68,12 @@ public final class R {
         public static final int info=0x7f050006;
         public static final int mensaje=0x7f050003;
         public static final int req=0x7f05000c;
+        public static final int tip1=0x7f05000e;
+        public static final int tip2=0x7f05000f;
+        public static final int tip3=0x7f050010;
         public static final int tips=0x7f050008;
+        /**  Activity Becas 
+         */
         public static final int title_activity_becas=0x7f050009;
         /**  Activity Inicio 
          */
@@ -74,6 +81,9 @@ public final class R {
         /**  Activity Manutención 
          */
         public static final int title_activity_manutencion=0x7f05000b;
+        /**  Activity Tips 
+         */
+        public static final int title_activity_tips=0x7f05000d;
     }
     public static final class style {
         /** 
