45f1646454fdf8c7994335580bba25f4ac364fad
VAIO-PC <VAIO-PC@Adriana>
2016-11-23 21:24:36 -0600

    comodo de imagenes

--- Beca/gen/com/example/beca/R.java
+++ Beca/gen/com/example/beca/R.java
@@ -30,15 +30,11 @@ public final class R {
         public static final int beca=0x7f020001;
         public static final int descarga=0x7f020002;
         public static final int fecha1=0x7f020003;
-        public static final int fechas=0x7f020004;
-        public static final int fondobca=0x7f020005;
-        public static final int fondobeca=0x7f020006;
-        public static final int fondoceleste=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int pagom=0x7f020009;
-        public static final int pagot=0x7f02000a;
-        public static final int templatebackground=0x7f02000b;
-        public static final int verdeagua=0x7f02000c;
+        public static final int fondobca=0x7f020004;
+        public static final int fondobeca=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int pagom=0x7f020007;
+        public static final int pagot=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080012;
