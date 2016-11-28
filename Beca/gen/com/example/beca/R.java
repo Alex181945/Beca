650f736c61fcd0119bda205001716fbf8d59d028
VAIO-PC <VAIO-PC@Adriana>
2016-11-28 16:50:43 -0600

    OK

--- Beca/gen/com/example/beca/R.java
+++ Beca/gen/com/example/beca/R.java
@@ -36,6 +36,7 @@ public final class R {
         public static final int pagom=0x7f020007;
         public static final int pagot=0x7f020008;
         public static final int pagotr=0x7f020009;
+        public static final int subes460x150x=0x7f02000a;
     }
     public static final class id {
         public static final int action_settings=0x7f080012;
