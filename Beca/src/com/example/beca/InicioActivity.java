c3e5d55285029e438a3ab3303121f529bf85e77a
Alex181945 <alestra55@hotmail.com>
2016-11-30 02:39:40 -0600

    ok

--- Beca/src/com/example/beca/InicioActivity.java
+++ Beca/src/com/example/beca/InicioActivity.java
@@ -18,8 +18,8 @@ public class InicioActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inicio);
 		
-		img = (ImageView) findViewById(R.id.iv1);
-		tv3 = (TextView) findViewById(R.id.tv3);
+		img = (ImageView)findViewById(R.id.iv1);
+		tv3 = (TextView)findViewById(R.id.tv3);
 	}
 	
 
@@ -36,7 +36,7 @@ public class InicioActivity extends Activity {
 		
 		if(tv3.getText().toString().equals(""))
 		{
-			img.setVisibility(0);
+			img.setVisibility(ImageView.INVISIBLE);
 		 a = "La Coordinación Nacional de Becas de Educación Superior (CNBES) C O N V O C A N,"
 				+ " a los/las estudiantes de Licenciatura y Técnico Superior Universitario (TSU)"
 				+ " que hayan ingresado o se encuentren realizando estudios en "
@@ -44,7 +44,7 @@ public class InicioActivity extends Activity {
 				+ "para que obtengan una beca de Manutención.";
 		}
 		
-		img.setVisibility(1);
+		img.setVisibility(ImageView.VISIBLE);
 		tv3.setText(a);
 		
 	}
